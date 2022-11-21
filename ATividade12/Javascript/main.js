ba849c62686cdd3cc6fb2538020173bad2acbbc4
Sheila  Santos <88636828+SheilaSantosPy@users.noreply.github.com>
2022-11-21 20:11:22 -0300

    Add files via upload

---
+++ ATividade12/Javascript/main.js
@@ -0,0 +1,14 @@
+
+function onClick(obj) {
+    //document.getElementById("imageoption").src = "/Images/janelaquebra.png";
+    obj.src ="/Images/janelaquebra.png";
+}
+
+
+function mMover(obj) {
+    obj.src ="/Images/janelaaberta.png";
+}
+
+function mOut(obj) {
+    obj.src ="/Images/janelafechada.png";
+}
