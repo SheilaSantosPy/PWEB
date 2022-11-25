97f4b5f3f1d89f5b0ccc1fcad3b47c992f0b4652
Sheila  Santos <88636828+SheilaSantosPy@users.noreply.github.com>
2022-11-25 01:42:38 -0300

    Delete Portifolio-principal directory

--- Portifolio-principal/Portifolio-principal/src/js/script.js
+++
@@ -1,9 +0,0 @@
-function redirectG() {
-    alert("Voce clicooou");
-    window.location.replace("https://github.com/SheilaSantosPy");
-}
-
-function redirectL() {
-    alert("Voce clicooou");
-    window.location.replace("https://www.linkedin.com/in/sheila-santos-328541b7/");
-}
