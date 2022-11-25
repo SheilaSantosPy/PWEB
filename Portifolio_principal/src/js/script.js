6fefff86bb0c079a53407da5fa22ea5b895d0f51
Sheila  Santos <88636828+SheilaSantosPy@users.noreply.github.com>
2022-11-25 01:44:02 -0300

    Add files via upload

---
+++ Portifolio_principal/src/js/script.js
@@ -0,0 +1,9 @@
+function redirectG() {
+    alert("Voce clicooou");
+    window.location.replace("https://github.com/SheilaSantosPy");
+}
+
+function redirectL() {
+    alert("Voce clicooou");
+    window.location.replace("https://www.linkedin.com/in/sheila-santos-328541b7/");
+}
